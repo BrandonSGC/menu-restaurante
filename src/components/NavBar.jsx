d137967c72202e1f061eeb076b102a61d53568f8
BrandonSGC <brangmz20@gmail.com>
2023-10-11 14:31:11 -0600

    Adding navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+export const NavBar = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="display">
+          <h1 className="logo">Restaurante</h1>
+          <nav>
+            <a href="#">Listado de Platillos</a>
+            <a href="#">Administración de Platillos</a>
+            <a href="#">Administración de Caregorías</a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
